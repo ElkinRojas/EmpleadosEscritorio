233348f19b7f7499bd3dfc23cdbcc84e6c855558
ElkinRojas <81581812+ElkinRojas@users.noreply.github.com>
2021-11-30 09:33:09 -0500

    Se agrega el proyecto

---
+++ EmpleadosEscritorio2/datos/Conexion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpleadosEscritorio2.datos
+{
+    internal class Conexion
+    {
+        SqlConnection con;
+
+        public Conexion()
+        {
+            con = new SqlConnection("Server=DESKTOP-MS73491;Database=empleado_bd;Integrated security=true");
+        }
+
+        public SqlConnection conectar()
+        {
+            try
+            { 
+                con.Open();
+                return con;
+            } catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public bool desconectar()
+        {
+            try
+            {
+                con.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
